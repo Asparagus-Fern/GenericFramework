0cde63dee015b708168ab20a65c5ae8962888f6e
taiye <604602270@qq.com>
2024-11-20 11:21:00 +0800

    新建GIS通用插件

---
+++ DevRuntime/Source/GIS/GISUtilities/Private/GISUtilities.cpp
@@ -0,0 +1,17 @@
+﻿#include "GISUtilities.h"
+
+#define LOCTEXT_NAMESPACE "FGISUtilitiesModule"
+
+void FGISUtilitiesModule::StartupModule()
+{
+	ICommonModuleInterface::StartupModule();
+}
+
+void FGISUtilitiesModule::ShutdownModule()
+{
+	ICommonModuleInterface::ShutdownModule();
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FGISUtilitiesModule, GISUtilities)
