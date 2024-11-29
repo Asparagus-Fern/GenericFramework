6e5a8df6e88ecaa9084374b46944151bf1ffda6d
taiye <604602270@qq.com>
2024-11-29 18:41:50 +0800

    插件目录结构更新，移除DevRuntime和DevEditor，增加DevModules，修复打包报错

---
+++ DevModules/WidgetModule/Source/Runtime/ScreenWidgetGeneration/Private/Group/CommonButton.cpp
@@ -0,0 +1,129 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Group/CommonButton.h"
+
+#include "ScreenWidgetManager.h"
+#include "Blueprint/WidgetTree.h"
+
+UCommonButton::UCommonButton(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bSelectable = true;
+	bApplyAlphaOnDisable = false;
+
+	static ConstructorHelpers::FClassFinder<UCommonButtonStyle> StyleClassFinder(TEXT("/Script/Engine.Blueprint'/WidgetModule/ButtonStyle/CBS_Transparent.CBS_Transparent_C'"));
+	if (StyleClassFinder.Succeeded())
+	{
+		Style = StyleClassFinder.Class;
+	}
+
+	SetVisibilityInternal(ESlateVisibility::Visible);
+}
+
+TSharedRef<SWidget> UCommonButton::RebuildWidget()
+{
+	return Super::RebuildWidget();
+}
+
+bool UCommonButton::Initialize()
+{
+	return Super::Initialize();
+}
+
+bool UCommonButton::InitializeForce()
+{
+	return UCommonButtonBase::Initialize();
+}
+
+void UCommonButton::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+}
+
+void UCommonButton::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UCommonButton::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void UCommonButton::NativeOnHovered()
+{
+	Super::NativeOnHovered();
+	HandleResponse(ECommonButtonResponseEvent::OnHovered);
+}
+
+void UCommonButton::NativeOnUnhovered()
+{
+	Super::NativeOnUnhovered();
+	HandleResponse(ECommonButtonResponseEvent::OnUnHovered);
+}
+
+void UCommonButton::NativeOnClicked()
+{
+	Super::NativeOnClicked();
+	HandleResponse(ECommonButtonResponseEvent::OnClick);
+}
+
+void UCommonButton::NativeOnPressed()
+{
+	Super::NativeOnPressed();
+	HandleResponse(ECommonButtonResponseEvent::OnPressed);
+}
+
+void UCommonButton::NativeOnReleased()
+{
+	Super::NativeOnReleased();
+	HandleResponse(ECommonButtonResponseEvent::OnReleased);
+}
+
+void UCommonButton::NativeOnSelected(bool bBroadcast)
+{
+	Super::NativeOnSelected(bBroadcast);
+	HandleResponse(ECommonButtonResponseEvent::OnSelected);
+}
+
+void UCommonButton::NativeOnDeselected(bool bBroadcast)
+{
+	Super::NativeOnDeselected(bBroadcast);
+	HandleResponse(ECommonButtonResponseEvent::OnDeselected);
+}
+
+void UCommonButton::NativeOnActived()
+{
+	IProcedureInterface::NativeOnActived();
+	IProcedureInterface::Execute_OnActived(this);
+}
+
+void UCommonButton::NativeOnInactived()
+{
+	IProcedureInterface::NativeOnInactived();
+	IProcedureInterface::Execute_OnInactived(this);
+}
+
+void UCommonButton::NativeOnCreate()
+{
+	IProcedureBaseInterface::NativeOnCreate();
+	IProcedureBaseInterface::Execute_OnCreate(this);
+}
+
+void UCommonButton::NativeOnRefresh()
+{
+	IProcedureBaseInterface::NativeOnRefresh();
+	IProcedureBaseInterface::Execute_OnRefresh(this);
+}
+
+void UCommonButton::NativeOnDestroy()
+{
+	IProcedureBaseInterface::NativeOnDestroy();
+	IProcedureBaseInterface::Execute_OnDestroy(this);
+}
+
+void UCommonButton::HandleResponse(ECommonButtonResponseEvent InResponseEvent)
+{
+	OnButtonResponse.Broadcast(this, InResponseEvent);
+}
---
+++ DevModules/WidgetModule/Source/Runtime/ScreenWidgetGeneration/Private/UserWidget/Menu/MenuContainer.cpp
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UserWidget/Menu/MenuContainer.h"
+
+#include "ScreenWidgetManager.h"
+#include "Group/CommonButtonGroup.h"
+#include "UserWidget/Menu/MenuStyle.h"
+
+UMenuContainer::UMenuContainer(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	DesignMenuStyleClass = LoadClass<UUserWidget>(nullptr, TEXT("/Script/UMGEditor.WidgetBlueprint'/WidgetModule/Widget/MenuStyle/WBP_Style_Example.WBP_Style_Example_C'"));
+#endif
+}
+
+void UMenuContainer::ConstructMenuContainer_Implementation(UMenuStyle* MenuStyle, int32 Index)
+{
+}
+
+void UMenuContainer::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+#if WITH_EDITOR
+	if (IsDesignTime())
+	{
+		for (const auto& DesignMenuStyle : DesignMenuStyles)
+		{
+			DestructMenuContainer(DesignMenuStyle);
+			DesignMenuStyle->MarkAsGarbage();
+		}
+
+		DesignMenuStyles.Reset();
+
+		if (IsValid(DesignMenuStyleClass))
+		{
+			for (int Index = 0; Index < DesignMenuNum; Index++)
+			{
+				UMenuStyle* NewDesignMenuStyle = CreateWidget<UMenuStyle>(this, DesignMenuStyleClass);
+				DesignMenuStyles.Add(NewDesignMenuStyle);
+				ConstructMenuContainer(NewDesignMenuStyle, Index);
+			}
+		}
+	}
+#endif
+}
+
+void UMenuContainer::NativeOnCreate()
+{
+	Super::NativeOnCreate();
+
+	CommonButtonGroup = NewObject<UCommonButtonGroup>(this, "CommonButtonGroup");
+	MenuStyles.Reset();
+}
+
+void UMenuContainer::NativeConstructMenuContainer(UMenuStyle* MenuStyle, int32 Index)
+{
+	if (!IsValid(MenuStyle) || MenuStyles.Contains(MenuStyle))
+	{
+		DLOG(DLogUI, Error, TEXT("Fail To Construct Menu Container"))
+		return;
+	}
+
+	MenuStyles.Add(MenuStyle);
+
+	/* 将菜单加入到CommonButtonGroup管理 */
+	if (bIsManagedByGroup && IsValid(MenuStyle->ActiveCommonButton))
+	{
+		CommonButtonGroup->AddWidget(MenuStyle->ActiveCommonButton);
+	}
+
+	ConstructMenuContainer(MenuStyle, Index);
+}
+
+void UMenuContainer::DestructMenuContainer_Implementation(UMenuStyle* MenuStyle)
+{
+}
+
+void UMenuContainer::NativeDestructMenuContainer(UMenuStyle* MenuStyle)
+{
+	if (!IsValid(MenuStyle) || !MenuStyles.Contains(MenuStyle))
+	{
+		DLOG(DLogUI, Error, TEXT("Fail To Construct Menu Container"))
+		return;
+	}
+
+	DestructMenuContainer(MenuStyle);
+
+	MenuStyles.Remove(MenuStyle);
+
+	if (bIsManagedByGroup && IsValid(MenuStyle->ActiveCommonButton))
+	{
+		CommonButtonGroup->RemoveWidget(MenuStyle->ActiveCommonButton);
+	}
+}
+
+void UMenuContainer::PostConstructMenuContainer_Implementation()
+{
+}
+
+void UMenuContainer::NativePostConstructMenuContainer()
+{
+	PostConstructMenuContainer();
+}
