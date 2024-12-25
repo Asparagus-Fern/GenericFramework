58d7bf76650d8245e4d5692404e4c6e995cad03d
taiye <604602270@qq.com>
2024-12-25 17:51:31 +0800

    同步

---
+++ DevModules/UtilityModule/Source/Runtime/UltraDynamicSkyUtilities/Private/UltraDynamicSkyUtilities.cpp
@@ -0,0 +1,15 @@
+﻿#include "UltraDynamicSkyUtilities.h"
+
+#define LOCTEXT_NAMESPACE "FUltraDynamicSkyUtilitiesModule"
+
+void FUltraDynamicSkyUtilitiesModule::StartupModule()
+{
+}
+
+void FUltraDynamicSkyUtilitiesModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FUltraDynamicSkyUtilitiesModule, UltraDynamicSkyUtilities)
---
+++ DevModules/UtilityModule/Source/Runtime/UltraDynamicSkyUtilities/UltraDynamicSkyUtilities.Build.cs
@@ -0,0 +1,29 @@
+﻿using UnrealBuildTool;
+
+public class UltraDynamicSkyUtilities : ModuleRules
+{
+	public UltraDynamicSkyUtilities(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"UltraDynamicSky"
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+
+				"DeveloperSettings"
+			}
+		);
+	}
+}
